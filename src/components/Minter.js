cfcec2bf7f3f7d18a05591268ffa92cdb5071df8
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-06-30 18:12:37 -0700

    added minting functionality for robocop and alien

--- src/components/Minter.js
+++ src/components/Minter.js
@@ -8,8 +8,8 @@ import alienPosterNFT from '../AlienPoster.json';
 import robocopPosterNFT from '../RobocopPoster.json';
 import spaceOdysseyPosterNFT from '../SpaceOdysseyPoster.json';
 
-const alienPosterAddress = '0x2AD4344563f9eA73a89B1d3665058c185a781CB6'
-const robocopPosterAddress = '0x20822036dDC00eaE0cA917897E0bbFb8a01c29F7'
+const alienAddress = '0x2AD4344563f9eA73a89B1d3665058c185a781CB6'
+const robocopAddress = '0x20822036dDC00eaE0cA917897E0bbFb8a01c29F7'
 const spaceOdysseyAddress = '0x244D105ecb52713B1cB971db7589d617D8FEb5f3'
 
 function Minter(props) {
@@ -36,6 +36,46 @@ function Minter(props) {
         }
     }
 
+    async function handleAlienMint() {
+        if (window.ethereum) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(
+                alienAddress,
+                alienPosterNFT.abi,
+                signer
+            );
+            try {
+                const response = await contract.mint(BigNumber.from(mintAmount), {
+                    value: ethers.utils.parseEther((0.01 * mintAmount).toString())
+                });
+                console.log('response: ', response);
+            } catch (err) {
+                console.log('error: ', err)
+            }
+        }
+    }
+
+    async function handleRobocopMint() {
+        if (window.ethereum) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(
+                robocopAddress,
+                robocopPosterNFT.abi,
+                signer
+            );
+            try {
+                const response = await contract.mint(BigNumber.from(mintAmount), {
+                    value: ethers.utils.parseEther((0.01 * mintAmount).toString())
+                });
+                console.log('response: ', response);
+            } catch (err) {
+                console.log('error: ', err)
+            }
+        }
+    }
+
     return (
         <div className='minter'>
             <Poster
@@ -43,16 +83,16 @@ function Minter(props) {
                 onClick={handleSpaceOdysseyMint} 
                 mintAmount={mintAmount}
                 setMintAmount={setMintAmount}
-                accounts={props.accounts}
-                setAccounts={props.setAccounts}
                 />
             <Poster 
                 src={alien}
+                onClick={handleAlienMint}
                 mintAmount={mintAmount}
                 setMintAmount={setMintAmount}
                 />
             <Poster 
                 src={robocop}
+                onClick={handleRobocopMint}
                 mintAmount={mintAmount}
                 setMintAmount={setMintAmount}
                 />
